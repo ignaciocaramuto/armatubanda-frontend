a49eb1e13173d6b8ac94bac332a69fd1329fdd2d
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-05-22 17:54:21 -0300

    update: nuevo componente list

---
+++ src/app/modules/list/list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ListModule { }
