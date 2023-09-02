a97ea7fd14f6d0de6a1c8562e921370be9cd73f5
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-09-02 15:11:59 -0300

    fix imports

--- src/app/modules/profile-creation/pages/creation-form/creation-form.component.ts
+++ src/app/modules/profile-creation/pages/creation-form/creation-form.component.ts
@@ -1,47 +1,65 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FormControl,FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
-import { BasicProfile, Instrument, MusicianContactInformation } from '../../interfaces/profile-creation.interface';
-import { environment } from 'src/environments/environment.development';
+import {
+  BasicProfile,
+  Instrument,
+  MusicianContactInformation,
+} from '../../interfaces/profile-creation.interface';
+import { environment } from 'src/environments/environment.local';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-creation-form',
   templateUrl: './creation-form.component.html',
-  styleUrls: ['./creation-form.component.scss']
+  styleUrls: ['./creation-form.component.scss'],
 })
 export class CreationFormComponent implements OnInit {
-
   private fb = inject(FormBuilder);
   private router = inject(Router);
-  private readonly baseUrl: string = environment.baseUrl;
+  private readonly baseUrl: string = environment.apiUrl;
   private http = inject(HttpClient);
 
   data: Instrument[] = [];
 
-  public creationProfileForm : FormGroup = this.fb.group({
-    firstName:['martin',[Validators.required]],
-    lastName:['pereyra',[Validators.required]],
-    stageName:['tnc',[]],
-    country:['argentina',[]],
-    city:['rosario',[]],
-    phoneNumber:['3416511155',[]],
-    webSite:['wwwmp.com',[]],
-    socialMediaWebSite:['wwwlinkedicom',[]],
-    bio:['esta es mi bio',[Validators.maxLength(256)]],
-    instrumentList:['',[]]
-
-  })
+  public creationProfileForm: FormGroup = this.fb.group({
+    firstName: ['martin', [Validators.required]],
+    lastName: ['pereyra', [Validators.required]],
+    stageName: ['tnc', []],
+    country: ['argentina', []],
+    city: ['rosario', []],
+    phoneNumber: ['3416511155', []],
+    webSite: ['wwwmp.com', []],
+    socialMediaWebSite: ['wwwlinkedicom', []],
+    bio: ['esta es mi bio', [Validators.maxLength(256)]],
+    instrumentList: ['', []],
+  });
 
   ngOnInit(): void {
     this.getInstruments();
   }
 
-  public onSubmit(){
-    if(this.creationProfileForm.valid){
-      const {firstName,lastName,stageName,country,city,phoneNumber,webSite,socialMediaWebSite,bio,instrumentList} = this.creationProfileForm.value;
-      const musicianContactObject:MusicianContactInformation = {
+  public onSubmit() {
+    if (this.creationProfileForm.valid) {
+      const {
+        firstName,
+        lastName,
+        stageName,
+        country,
+        city,
+        phoneNumber,
+        webSite,
+        socialMediaWebSite,
+        bio,
+        instrumentList,
+      } = this.creationProfileForm.value;
+      const musicianContactObject: MusicianContactInformation = {
         name: firstName,
         lastname: lastName,
         stageName: stageName,
@@ -50,33 +68,29 @@ export class CreationFormComponent implements OnInit {
         city: city,
         phoneNumber: phoneNumber,
         webSite: webSite,
-        socialMediaLink: socialMediaWebSite
-      }
+        socialMediaLink: socialMediaWebSite,
+      };
       const instruments: Instrument[] = instrumentList;
-      const basicInfo:BasicProfile = {
+      const basicInfo: BasicProfile = {
         musicianContactInformation: musicianContactObject,
-        instruments:instruments
-      }
+        instruments: instruments,
+      };
       console.log(basicInfo);
-      const urlPut = `${this.baseUrl}/musician/create-profile`
-      console.log(this.http.put<BasicProfile>(urlPut,basicInfo));
-    }
-    else{
+      const urlPut = `${this.baseUrl}/musician/create-profile`;
+      console.log(this.http.put<BasicProfile>(urlPut, basicInfo));
+    } else {
       console.log('There is an error');
     }
   }
-
 
   getInstruments(): void {
     const url = `${this.baseUrl}/instrument/all`;
 
     this.http.get<Instrument[]>(url).subscribe({
-
-      next: (list) => {this.data = list;},
+      next: (list) => {
+        this.data = list;
+      },
       error: (e) => console.log(e),
-    }
-    );
+    });
   }
-
-
 }
--- src/app/modules/profile-creation/pages/instrument-form/instrument-form.component.ts
+++ src/app/modules/profile-creation/pages/instrument-form/instrument-form.component.ts
@@ -1,52 +1,50 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { environment } from 'src/environments/environment.development';
-import { Instrument, MusicianContactInformation } from '../../interfaces/profile-creation.interface';
+import { environment } from 'src/environments/environment.local';
+import {
+  Instrument,
+  MusicianContactInformation,
+} from '../../interfaces/profile-creation.interface';
 import { HttpClient } from '@angular/common/http';
 import { Observable, delay } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
-
 @Component({
   selector: 'app-instrument-form',
   templateUrl: './instrument-form.component.html',
-  styleUrls: ['./instrument-form.component.scss']
+  styleUrls: ['./instrument-form.component.scss'],
 })
 export class InstrumentFormComponent implements OnInit {
-
   private route = inject(ActivatedRoute);
   private http = inject(HttpClient);
   private fb = inject(FormBuilder);
 
-  private readonly baseUrl: string = environment.baseUrl;
+  private readonly baseUrl: string = environment.apiUrl;
 
-  public InstrumentSelectionForm : FormGroup = this.fb.group({
-    instruments:['',[]]
+  public InstrumentSelectionForm: FormGroup = this.fb.group({
+    instruments: ['', []],
   });
 
   data: Instrument[] = [];
-  musicianBasicInfo: MusicianContactInformation|undefined = undefined;
+  musicianBasicInfo: MusicianContactInformation | undefined = undefined;
 
   ngOnInit(): void {
     this.getInstruments();
-    this.route.paramMap.subscribe(params => {
-    const myObject = window.history.state;
-    this.musicianBasicInfo = myObject;
-  }
-    )
+    this.route.paramMap.subscribe((params) => {
+      const myObject = window.history.state;
+      this.musicianBasicInfo = myObject;
+    });
     console.log(this.musicianBasicInfo);
-
   }
 
   getInstruments(): void {
     const url = `${this.baseUrl}/instrument/all`;
 
     this.http.get<Instrument[]>(url).subscribe({
-
-      next: (list) => {this.data = list;},
+      next: (list) => {
+        this.data = list;
+      },
       error: (e) => console.log(e),
-    }
-    );
+    });
   }
-
 }
