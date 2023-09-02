60e04117a9b0d76f90b05996797e55ae9a0697f2
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-09-02 15:07:01 -0300

    get Musicians

--- src/app/modules/list/services/list.service.ts
+++ src/app/modules/list/services/list.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Musician } from 'src/app/core/models/musician';
@@ -14,19 +14,7 @@ export class ListService {
   private http = inject(HttpClient);
 
   getAllUsers(): Observable<Musician[]> {
-    return this.http.get<Musician[]>(`${this.baseUrl}/musician`)
-  }
-
-  getUser(id: number): Observable<Musician> {
-    return of({
-      id: 1,
-      firstname: 'Fabricio',
-      lastname: 'Gomez',
-      role: 'Guitarrista',
-      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
-      image: 'https://i.ibb.co/9HD3gMf/img-random.png',
-      rate: 4.5,
-      opinions: 15
-    });
+    let params = new HttpParams();
+    return this.http.get<Musician[]>(`${this.baseUrl}/musician`, {params})
   }
 }
