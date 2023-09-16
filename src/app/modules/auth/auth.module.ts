43a2afefb05a81816318b0e3d12be80e9bd1d622
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-09-16 11:41:40 -0300

    imported inputtext

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -6,9 +6,16 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { MatSelectModule } from '@angular/material/select';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { InputTextComponent } from 'src/app/core/components/input-text/input-text.component';
 
 @NgModule({
   declarations: [LoginPageComponent, RegisterPageComponent],
-  imports: [CommonModule, AuthRoutingModule, MatSelectModule, SharedModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MatSelectModule,
+    SharedModule,
+    InputTextComponent,
+  ],
 })
 export class AuthModule {}
