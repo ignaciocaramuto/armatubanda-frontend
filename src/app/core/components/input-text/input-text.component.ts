dfa875f6a60fb91106b5e56c03651c60d8559f20
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-09-16 11:21:23 -0300

    New generic input text

---
+++ src/app/core/components/input-text/input-text.component.ts
@@ -0,0 +1,35 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Injector,
+  Input,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { FormControlValueAccessorConnector } from '../form-control-value-accessor-connector/form-control-value-accessor-connector';
+import { NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-text',
+  standalone: true,
+  imports: [CommonModule, MatInputModule, ReactiveFormsModule],
+  templateUrl: './input-text.component.html',
+  styleUrls: ['./input-text.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: InputTextComponent,
+      multi: true,
+    },
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InputTextComponent extends FormControlValueAccessorConnector {
+  @Input() placeholder: string = '';
+  @Input() required: boolean = false;
+  @Input() type: string = 'text';
+
+  constructor(injector: Injector) {
+    super(injector);
+  }
+}
