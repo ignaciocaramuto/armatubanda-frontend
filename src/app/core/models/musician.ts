1b1d6d8db77a4d00d3434c81edc09bc683ba994f
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-09-23 14:57:24 -0300

    Update musician model

--- src/app/core/models/musician.ts
+++ src/app/core/models/musician.ts
@@ -1,23 +1,74 @@
-import { Instrument } from "./instrument.interface";
-import { ProfileImage } from "./profile-image.interface";
-import { Review } from "./review.interface";
+import { ExperienceType } from '../enums/experienceType.enum';
+import { Instrument } from './instrument.interface';
+import { ProfileImage } from './profile-image.interface';
+import { Review } from './review.interface';
 
 export interface Musician {
-    id: number;
-    musicianContactInformation: ContactInformation;
-    intruments: Instrument[];
-    profileImage: ProfileImage;
-    reviews: Review[]
+  id: number;
+  personalInformation: PersonalInformation;
+  contactInformation: ContactInformation;
+  skillsInformation: SkillsInformation;
+  educationInformation: EducationInformation;
+  careerInformation: CareerInformation;
+  biographyInformation: BiographyInformation;
+  preferenceInformation: PreferenceInformation;
+  profileImage: ProfileImage;
+  reviews: Review[];
+}
+
+export interface PersonalInformation {
+  name: string;
+  lastname: string;
+  stageName: string;
+  birthday: string;
+  gender: string;
+  country: string;
+  city: string;
 }
 
 export interface ContactInformation {
-    name: string;
-    lastname: string;
-    stageName: string;
-    bio: string;
-    country: string;
-    city: string;
-    phoneNumber: string;
-    webSite: string;
-    socialMediaLink: string;
+  phoneNumber: string;
+  webSite: string;
+  socialMedia: string;
+}
+
+export interface SkillsInformation {
+  instrumentExperience: InstrumentExperience[];
+}
+
+export interface InstrumentExperience {
+  instrument: Instrument;
+  experience: ExperienceType;
+}
+
+export interface EducationInformation {
+  educationHistory: EducationHistory[];
+}
+
+export interface EducationHistory {
+  name: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+}
+
+export interface CareerInformation {
+  careerHistory: CareerHistory[];
+}
+
+export interface CareerHistory {
+  name: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+}
+
+export interface BiographyInformation {
+  bio: string;
+}
+
+export interface PreferenceInformation {
+  lookingBands: boolean;
+  lookingMusician: boolean;
+  available: boolean;
 }
