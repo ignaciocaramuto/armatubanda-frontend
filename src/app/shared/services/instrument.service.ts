075079237a5087d01f08a14d466260219333721f
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-10-21 12:01:00 -0300

    Added instrument service

---
+++ src/app/shared/services/instrument.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Instrument } from 'src/app/core/models/instrument.interface';
+import { CrudService } from 'src/app/core/services/crud.service';
+import { environment } from 'src/environments/environment.local';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InstrumentService extends CrudService<Instrument> {
+  constructor(http: HttpClient) {
+    super(http, `${environment.apiUrl}/instrument`);
+  }
+
+  getInstruments(): Observable<Instrument[]> {
+    return this.getAll();
+  }
+
+  addInstrument(instrument: Instrument): Observable<Instrument> {
+    return this.create(instrument);
+  }
+}
