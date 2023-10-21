e62decac150515e5fc3e956c972b12895f0c2ba2
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-10-21 12:04:03 -0300

    updated list service extends crud service

--- src/app/modules/list/services/list.service.ts
+++ src/app/modules/list/services/list.service.ts
@@ -1,13 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Musician } from 'src/app/core/models/musician';
 import { CrudService } from 'src/app/core/services/crud.service';
+import { environment } from 'src/environments/environment.local';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ListService extends CrudService<Musician[]> {
+export class ListService extends CrudService<Musician> {
+  constructor(http: HttpClient) {
+    super(http, `${environment.apiUrl}/musician`);
+  }
+
   getAllUsers(filters?: any): Observable<Musician[]> {
-    return this.get('musician/all', filters);
+    return this.getAll(filters);
   }
 }
