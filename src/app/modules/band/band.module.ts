53767a101ae89c1d20609288791b28ef2ce0483c
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-01-03 22:08:52 -0300

    bands module

---
+++ src/app/modules/band/band.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BandRoutingModule } from './band-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BandRoutingModule
+  ]
+})
+export class BandModule { }
