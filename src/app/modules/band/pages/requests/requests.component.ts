0782f92ca93877d969bf0efc0630fac003c9bde3
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-01-04 22:47:21 -0300

    band request mockup

--- src/app/modules/band/pages/requests/requests.component.ts
+++ src/app/modules/band/pages/requests/requests.component.ts
@@ -1,9 +1,14 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ProfileImageComponent } from 'src/app/core/components/profile-image/profile-image.component';
 
 @Component({
   selector: 'app-requests',
   templateUrl: './requests.component.html',
   styleUrls: ['./requests.component.scss'],
   standalone: true,
+  imports: [ProfileImageComponent, MatCardModule, MatButtonModule, NgFor],
 })
 export class RequestsComponent {}
