3d871197c7c933a30bc2ec995927bd34d35f8fe0
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-01-06 16:02:26 -0300

    wip: advertisements

--- src/app/modules/advertisements/pages/advertisements-page/advertisements-page.component.ts
+++ src/app/modules/advertisements/pages/advertisements-page/advertisements-page.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FiltersComponent } from 'src/app/modules/list/components/filters/filters.component';
+import { MatCardModule } from '@angular/material/card';
+import { ProfileImageComponent } from 'src/app/core/components/profile-image/profile-image.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-advertisements-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FiltersComponent,
+    ProfileImageComponent,
+    MatCardModule,
+    MatButtonModule,
+  ],
   templateUrl: './advertisements-page.component.html',
-  styleUrls: ['./advertisements-page.component.scss']
+  styleUrls: ['./advertisements-page.component.scss'],
 })
-export class AdvertisementsPageComponent {
-
-}
+export class AdvertisementsPageComponent {}
