692fb50a232a70e610157a72204d966e91914e3d
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-01-08 22:14:12 -0300

    wip: band creation form

--- src/app/modules/band/pages/create-band-profile/create-band-profile.component.ts
+++ src/app/modules/band/pages/create-band-profile/create-band-profile.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { InputTextComponent } from 'src/app/core/components/input-text/input-text.component';
 
 @Component({
   selector: 'app-create-band-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    InputTextComponent,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './create-band-profile.component.html',
-  styleUrls: ['./create-band-profile.component.scss']
+  styleUrls: ['./create-band-profile.component.scss'],
 })
 export class CreateBandProfileComponent {
+  private fb = inject(FormBuilder);
 
+  bandInfoFormGroup: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    description: ['', Validators.required],
+    country: ['', Validators.required],
+    city: ['', Validators.required],
+    phoneNumber: ['', Validators.required],
+    webSite: ['', Validators.required],
+    socialMedia: ['', Validators.required],
+    creationDate: ['', Validators.required],
+  });
 }
