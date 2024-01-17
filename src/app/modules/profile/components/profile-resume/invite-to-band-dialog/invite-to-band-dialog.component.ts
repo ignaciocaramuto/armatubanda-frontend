b5a81e94aa2a791d2d0e5593692fc15e32c2c772
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-01-17 20:42:18 -0300

    added mat list

--- src/app/modules/profile/components/profile-resume/invite-to-band-dialog/invite-to-band-dialog.component.ts
+++ src/app/modules/profile/components/profile-resume/invite-to-band-dialog/invite-to-band-dialog.component.ts
@@ -1,20 +1,16 @@
-import {
-  Component,
-  ChangeDetectionStrategy,
-  OnInit,
-  inject,
-} from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { DialogComponent } from 'src/app/core/components/dialog/dialog.component';
 import { MusicianBands } from 'src/app/core/models/musicianBands.interface';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ProfileService } from '../../../services/profile.service';
 import { ProfileImageComponent } from 'src/app/core/components/profile-image/profile-image.component';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-invite-to-band-dialog',
   standalone: true,
-  imports: [NgFor, DialogComponent, ProfileImageComponent],
+  imports: [NgFor, DialogComponent, ProfileImageComponent, MatListModule],
   templateUrl: './invite-to-band-dialog.component.html',
   styleUrls: ['./invite-to-band-dialog.component.scss'],
 })
@@ -28,5 +24,6 @@ export class InviteToBandDialogComponent implements OnInit {
       .getMusicianLeaderBands(this.userId)
       .subscribe((result) => (this.bands = result));
   }
+
   confirmBandInvitation(): void {}
 }
