c33926f04663fcf6eeb3fc85c2abeef8ff616044
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-02-04 22:45:57 -0300

    no posts text

--- src/app/modules/profile/components/profile-feed/profile-feed.component.ts
+++ src/app/modules/profile/components/profile-feed/profile-feed.component.ts
@@ -1,14 +1,14 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Post } from '../../models/post.interface';
 import { PostComponent } from './post/post.component';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-profile-feed',
   templateUrl: './profile-feed.component.html',
   styleUrls: ['./profile-feed.component.scss'],
   standalone: true,
-  imports: [NgFor, PostComponent],
+  imports: [NgFor, PostComponent, NgIf],
 })
 export class ProfileFeedComponent {
   @Input() posts: Post[] = [];
