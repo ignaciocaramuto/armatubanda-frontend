bdc8fe8d999c1439a6005552743cf7ca2cf157ce
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-02-20 10:50:52 -0300

    fixed prod apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'rest-armatubanda-production.up.railway.app/api',
+  apiUrl: 'http://rest-armatubanda-production.up.railway.app/api',
 };
