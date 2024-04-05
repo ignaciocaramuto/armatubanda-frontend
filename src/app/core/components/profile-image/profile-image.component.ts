d2b4cae12fe1229bc63946a33d89ba2b39bf12c4
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-04-05 17:39:47 -0300

    removed unnecesary pipe

--- src/app/core/components/profile-image/profile-image.component.ts
+++ src/app/core/components/profile-image/profile-image.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SanitizeImagePipe } from '../../pipes/sanitize-image.pipe';
 import { environment } from 'src/environments/environment.local';
 
 @Component({
   selector: 'app-profile-image',
   standalone: true,
-  imports: [CommonModule, SanitizeImagePipe],
+  imports: [CommonModule],
   templateUrl: './profile-image.component.html',
   styleUrls: ['./profile-image.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/core/pipes/sanitize-image.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SanitizeImagePipe } from './sanitize-image.pipe';
-
-describe('SanitizeImagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SanitizeImagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
